765c1a81db3fdf2629d2d85df5b87826615e1dc7
maaarcoo13 <dal428695@utdallas.edu>
2025-10-17 18:08:22 -0500

    Profile Page creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './login/login.page';
+import { HomePage } from './home/home.page';
+import { ProfilePage } from './profile/profile.page';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'login',
-    pathMatch: 'full',
+    pathMatch: 'full'
   },
   {
     path: 'login',
-    loadComponent: () => import('./login/login.page').then(m => m.LoginPage),
+    component: LoginPage  
   },
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then(m => m.HomePage),
+    component: HomePage
   },
+  {
+    path: 'profile',
+    component: ProfilePage
+  }
 ];
